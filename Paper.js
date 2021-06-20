badd576e43befcdf8aef86fd1812d545d414560b
EeshanLakhmale <76906760+EeshanLakhmale@users.noreply.github.com>
2021-06-20 23:43:23 +0530

    Add files via upload

---
+++ Paper.js
@@ -0,0 +1,38 @@
+class Paper
+{
+	constructor(x,y,r)
+	{
+		var options={
+			isStatic:false,
+            restitution: 0.3,
+            friction:0.5,
+            density:1.2			
+			}
+
+			this.image = loadImage("paper.png")
+
+			this.x = x
+			this.y = y
+			this.r = r
+	
+		this.body=Bodies.circle(x, y,(this.r -20)/2, options);
+ 		World.add(world, this.body);
+
+	}
+	display()
+	{
+			
+			var paperPos=this.body.position;		
+
+			push()
+			translate(paperPos.x, paperPos.y);
+			//strokeWeight(4);
+			fill(128,128,128)
+			imageMode(CENTER);
+			image(this.image, 0,0,this.r,this.r)
+			//circle(0,0,this.radius);
+			pop()
+			
+	}
+
+}
